c347498f6281f936865a9537b19baddb5107692c
Robert Stiffler (SunGard K-12 Bethlehem) <robert.stiffler@Bethlehem.Sungardps.lcl>
2017-04-06 12:26:07 -0400

    Added route for getting weather conditions

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -2,6 +2,7 @@ var express, router;
 
 express = require('express');
 router = express.Router();
+wunderground = require('../weatherapis/wunderground');
 
 /**
  * Ping test method
@@ -11,6 +12,16 @@ router = express.Router();
  */
 router.get('/ping', function(req, res) {
   res.json({ ping: 'pong' });
+});
+
+/**
+ * Ping test method
+ * @param  {Object} req the Request object
+ * @param  {Object} res the Response object
+ * @return JSON Object { conditions: {} }
+ */
+router.get('/conditions/:zipcode', function(req, res){
+  res.json({ conditions: "test"});
 });
 
 module.exports = router;
