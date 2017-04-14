e3bf4689e6d51047df100cb75ff2bea0c14a7755
Caitlin Hoyle <cait@metaloma.com>
2017-04-14 18:00:23 -0400

    Implement server side validation, promise chaining, service layer

--- src/server/services/wunderground.js
+++ src/server/services/wunderground.js
@@ -8,6 +8,12 @@ config = require('../config.json');
   'use strict';
 
   /**
+   * Constants
+   */
+
+  const wundergroundapikey = config.wundergroundapikey;
+
+  /**
    * Models
    */
   const ErrorMessage = require('../models/errorMessage.js');
@@ -16,65 +22,56 @@ config = require('../config.json');
   /**
    * getConditions returns the weather conditions for a given zipcode
    * @param  {[string]} zipcode zipcode to return weather of
-   * @return {[Promise]}         Promise of weather condition data
+   * @return {[Promise]}         Promise for weather condition data
    */
   module.exports.getConditions = (zipcode) => {
+    const url = `http://api.wunderground.com/api/${wundergroundapikey}/conditions/q/${zipcode}.json`;
     return new Promise((resolve, reject) => {
-      if (!validZip(zipcode)) {
-        reject(new ErrorMessage(404, "Invalid zipcode"));
-      } else {
-        const wundergroundapikey = config.wundergroundapikey;
-        const url = `http://api.wunderground.com/api/${wundergroundapikey}/conditions/q/${zipcode}.json`;
+      try {
         http.get(url, (res) => {
           res.pipe(bl((err, data) => {
-            try {
-              let parsedData = JSON.parse(data);
-              if (parsedData.response.error) {
-                let message = parsedData.response.error.description;
-                reject(new ErrorMessage(404, message));
-              } else {
-                var condObject = new Conditions();
-                condObject.fromWunderground(parsedData);
-                console.log(condObject);
-                resolve({ parsedData: parsedData, condObject: condObject});
-              }
-            } catch(e) {
-              reject(new ErrorMessage(500, e.message));
+            let parsedData = JSON.parse(data);
+
+            // handle errors from Weather Underground
+            if (parsedData.response.error) {
+              let errMessage = parsedData.response.error.description;
+              return reject(new ErrorMessage(404, errMessage));
             }
+
+            // map Weather Underground data to Conditions model
+            let conditionsModel = new Conditions().fromWunderground(parsedData);
+            var result = JSON.stringify(conditionsModel);
+            resolve(result);
           }));
         });
+      } catch (e) {
+        reject(new ErrorMessage(500, e.message));
       }
     });
   };
 
   module.exports.getForecast = (zipcode) => {
+    const url = `http://api.wunderground.com/api/${wundergroundapikey}/forecast/geolookup/q/${zipcode}.json`;
     return new Promise((resolve, reject) => {
-      if (!validZip(zipcode)) {
-        reject(new ErrorMessage(404, "Invalid zipcode"));
-      } else {
-        const wundergroundapikey = config.wundergroundapikey;
-        const url = `http://api.wunderground.com/api/${wundergroundapikey}/forecast/geolookup/q/${zipcode}.json`;
+      try {
         http.get(url, (res) => {
           res.pipe(bl((err, data) => {
-            try {
-              let parsedData = JSON.parse(data);
-              if (parsedData.response.error) {
-                let message = parsedData.response.error.description;
-                reject(new ErrorMessage(404, message));
-              } else {
-                resolve(parsedData);
-              }
-            } catch (e) {
-              reject(new errorMessage(500, e.message));
+            let parsedData = JSON.parse(data);
+
+            // handle errors from Weather Underground
+            if (parsedData.response.error) {
+              let errMessage = parsedData.response.error.description;
+              return reject(new ErrorMessage(404, errMessage));
             }
+
+            // map Weather Underground data to Forecast model
+            // TODO
+            resolve(parsedData);
           }));
         });
+      } catch (e) {
+        reject(new ErrorMessage(500, e.message));
       }
     });
   };
-
-  var validZip = function(zipCode) {
-    return (zipCode !== null && /^\d{5}$/.test(zipCode));
-  };
-
 })();
